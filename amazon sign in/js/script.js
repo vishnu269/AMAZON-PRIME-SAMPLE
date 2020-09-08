bbf6605efc07b5f90eca81b4cb751fdaa2bc1146
Vishnu Kafle <infalliblevishnu@gmail.com>
2020-09-08 13:38:23 +0545

    first sample project

---
+++ amazon sign in/js/script.js
@@ -0,0 +1,22 @@
+// $(function() {
+
+//   $('a.pop').click(function(event) {
+//   	/* Act on the event */
+//   	e.preventDefault();
+
+  	
+
+//   	${'[rel="tooltip"]'} .tooltip();
+
+  		
+//   	}); 
+  	
+  		
+  
+ $(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
+
+
+// });
